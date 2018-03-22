f0fe4a0add3685d2337a588e6e410b59ba434b96
jonahkafeero <jjkafeero@gmail.com>
2018-03-22 23:18:02 +0300

    b

---
+++ accounting_formulae.py
@@ -0,0 +1,13 @@
+from .definitions import *
+
+total_assets = 0
+current_assets = []
+fixed_assets = []
+
+
+def Capital(total_assts, total_lias):
+    return total_assts - total_lias
+
+
+def total_assets():
+    total_assets = add_array(current_assets + fixed_assets)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='Gas-Station-Software',
+    version='0.1.0',
+    packages=['python3-minimal'],
+    url='https://github.com/jonahkaks/Gas-Station-Software',
+    license='MIT',
+    author='root',
+    author_email='jjkafeero@gmail.com',
+    description='An accounting software for gas stations')
